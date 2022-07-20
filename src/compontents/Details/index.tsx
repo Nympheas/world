55788f217d988c6aafee2fe64a5ff17aa0a4bc34
changyufei <changyufei@live.cn>
2022-07-20 14:46:53 +0800

    first commit

---
+++ src/compontents/Details/index.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export class Details extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>Details</div>
+    )
+  }
+}
+
+export default Details
---
+++ src/compontents/Home/index.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export class Home extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
+
+export default Home
